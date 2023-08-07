839c35416a414beb5923fbe23077e3acde7590f3
axelwise <axelwise676@gmail.com>
2023-08-07 17:25:35 +0100

    added next/auth to project

---
+++ src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,36 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "your-username"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "your-password"
+                }
+            },
+            async authorize(credentials) {
+                const user = { id: "1", name: "Axel", password: process.env.ADMIN_PASSWORD};
+
+                if(credentials.username === user.name && credentials.password === user.password) {
+                    return user;
+                } else {
+                    return null;
+                }
+            }
+        })
+    ]
+} 
