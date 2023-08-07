6912add16bf4afb0ff6c20b32a49cc4ec7037012
axelwise <axelwise676@gmail.com>
2023-08-07 19:45:46 +0100

    added styles

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -5,15 +5,15 @@ import { getServerSession } from "next-auth/next";
 export default async function Navbar() {
     const session = await getServerSession(options);
     return (
-        <nav>
-            <ul>
-                <li><Link href="/">Home</Link></li>
+        <nav class="">
+            <ul class="flex">
+                <li class="mr-6"><Link href="/" class="text-blue-500 hover:text-blue-800">Home</Link></li>
                 {session ? (
-                    <li><Link href="/api/auth/signout">Sign Out</Link></li>
+                    <li class="mr-6"><Link href="/api/auth/signout" class="text-blue-500 hover:text-blue-800">Sign Out</Link></li>
                     ) : (
-                    <li><Link href="/api/auth/signin">Sign In</Link></li>
+                    <li class="mr-6"><Link href="/api/auth/signin" class="text-blue-500 hover:text-blue-800">Sign In</Link></li>
                 )}
-                <li><Link href="/posts">Posts</Link></li>
+                <li class="mr-6"><Link href="/posts" class="text-blue-500 hover:text-blue-800">Posts</Link></li>
             </ul>
         </nav>
     );
